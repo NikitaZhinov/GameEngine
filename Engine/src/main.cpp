2ddf11621c097b68ef57294ccfaf43464b2fff58
Никита Жинов <zhinovnikita@yandex.ru>
2024-01-11 16:49:40 +0700

    added GLEW

--- Engine/src/main.cpp
+++ Engine/src/main.cpp
@@ -1,15 +1,21 @@
+#include <iostream>
+
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
 int main() {
     GLFWwindow* window;
 
     if (!glfwInit())
-        return -1;
+        std::runtime_error("Can't init GLFW!\n");
+
+    if (glewInit() != GLEW_OK)
+        std::runtime_error("Can't init GLEW!\n");
 
     window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
     if (!window) {
         glfwTerminate();
-        return -1;
+        std::runtime_error("Can't created window!\n");
     }
 
     glfwMakeContextCurrent(window);
